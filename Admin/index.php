bd6ed809222be5916157f9bcb39a6d403a55f7b4
ishak <ijmilou@roc-dev.com>
2021-10-28 10:51:56 +0200

    made some changes to the directory and moved some files

--- Admin/index.php
+++ Admin/index.php
@@ -1,24 +1,162 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link href="../template/style.css" rel="stylesheet">
-    <link href="admin.css" rel="stylesheet">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Admin_style.css">
+    <title>Previous Plannings</title>
+    <link rel="icon" href="../images/icon/favicon.ico" type="image/icon">
 </head>
 <body>
-<?php
-    require_once "../template/header.php";
-?>
+<header>
+    <img src="../images/logo.png" alt="Logo" id="logo">
+    <ul id="header">
+        <li class="navbar"><a href="">login</a></li>
+        <li class="navbar"><a href="">register</a></li>
+        <li class="navbar"><a href="">profile</a></li>
+    </ul>
+</header>
 
-<form id="formAdmin">
-    <label>Search for your name</label><br>
-    <select id="select">
-        <option value="leerling1">leerling 1</option>
-        <option value="leerling2">leerling 2</option>
-        <option value="leerling3">leerling 3</option>
-        <option value="leerling4">leerling 4</option>
-    </select>
-</form>
+<!--select option for admin to choose student/employee-->
+    <form id="formAdmin">
+        <label class="labelName">Division</label>
+        <select class="select">
+            <option>Division 1</option>
+            <option>Division 2</option>
+            <option>Division 3</option>
+            <option>Division 4</option>
+        </select>
+        <label class="labelName">Employee</label>
+            <select class="select">
+                <option>Employee 1</option>
+                <option>Employee 2</option>
+                <option>Employee 3</option>
+                <option>Employee 4</option>
+            </select>
+        <button type="button">Submit</button>
+    </form>
+
+<!--pre-plannings of the employee-->
+<div id="PrePlanning">
+    <h1 id="pagetitle">Previous Plannings</h1>
+    <div id="PlanningGrid">
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+
+        <div class="GridItem">
+            <ul class="ticketList">
+                <li><p class="ticket_title">PHP</p></li>
+                <li><p class="ticket_desc">- Web - 5 hours</p></li>
+            </ul>
+            <p>Date</p>
+            <p>26-10-2021</p>
+            <p>Start time:</p>
+            <p>09:30</p>
+            <p>Stop time:</p>
+            <p>16:30</p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
