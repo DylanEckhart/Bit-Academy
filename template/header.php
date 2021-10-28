3753f2eb9de7a6c2cd2b35ba0d185862706e9c56
Dylan Eckhart <deckhart@roc-dev.com>
2021-10-28 11:32:11 +0200

    Made links

--- template/header.php
+++ template/header.php
@@ -1,8 +1,8 @@
 <header>
     <img src="../images/logo.png" alt="Logo" id="logo">
     <ul id="header">
-        <li class="navbar"><a href="">login</a></li>
-        <li class="navbar"><a href="">register</a></li>
-        <li class="navbar"><a href="">profile</a></li>
+        <li class="navbar"><a href="../pre_planning/index.php">Overview</a></li>
+        <li class="navbar"><a href="../Submit/index.php">Submit</a></li>
+        <li class="navbar"><a href="../Admin/index.php">Admin</a></li>
     </ul>
 </header>
