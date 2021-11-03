07138f10d30490200f28170c9442c1eddfd22214
Dylan Eckhart <deckhart@roc-dev.com>
2021-11-03 09:16:41 +0100

    Database connection

---
+++ DB/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Connection inputs to MySQL database
+    $servername = "localhost";
+    $username = "id17762295_bitacademy";
+    $password = "Workboard-Team2";
+    $databaseName = "id17762295_bitacademydb";
+
+    // Connect to the database
+    $connection = mysqli_connect($servername, $username, $password, $databaseName);
+
+    // Check connection
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+    echo "Connected successfully";
+
