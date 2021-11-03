97661fc0843de9df486665e89d3557a347e72c34
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-11-03 13:19:59 +0100

    made a small change to the connection.php

--- DB/connection.php
+++ DB/connection.php
@@ -4,18 +4,19 @@ function openConn()
 {
     // Connection inputs to MySQL database
     $servername = "localhost";
-    $username = "id17762295_bitacademy";
+//    $username = "id17762295_bitacademy";
+    $username = "root";
     $password = "Workboard-Team2";
+//    $password = "root";
     $databaseName = "id17762295_bitacademydb";
 
     // Connect to the database
-    $connection = mysqli_connect($servername, $username, $password, $databaseName);
+    $connection = mysqli_connect($servername, $username, $databaseName);
 
     // Check connection
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
     }
-//    echo "Connected successfully";
+    echo "Connected successfully";
     return $connection;
 }
-
