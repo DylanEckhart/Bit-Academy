03ead7d9348de86063a112c85ff33c0aef6e7e44
Rick <Mcfly2340@outlook.com>
2021-11-08 14:47:42 +0100

    finished page layout

--- index.php
+++ index.php
@@ -14,39 +14,73 @@
 <h1 id="PageName">Submit</h1><br>
 
 <!--WARNINGBOX-->
-<div class="alert">
+<div class="alert" style="line-height: 20px">
     <span class="closebtn" onclick="this.parentElement.style.height='0'; this.parentElement.style.padding='0';">&#10006;</span>
-    Select a subject and a chapter. Then select the time of when to start and when to end <br>
-</div>
+    <label style="background-color: transparent; font-size: 30px" for=".alert">Description <br> <br></label>Choose A category and subject to add it to the planning
+    </div>
+
+
 <!--THIS WEEK PLANNING-->
 <form class="thisWeekPlanning">
-    <label class="PlanningHeader" style="background-color: transparent;">This week planning</label><br>
+    <label class="PlanningHeader">This week planning</label><br>
 
-    <ul id="listOfSubjects">
-        <li class="listItem">item1 - chapter 1 <br> 09:30 - 16:00</li>
-        <li class="listItem">item2 - chapter 3 <br> 09:30 - 16:00</li>
+    <ul id="listOfTasks">
+        <li class="listItem">item1 - chapter 1 <br>Start Time: 09:30 <br>
+            <button  id="Pause1" onclick="return hidePauseButton()">Pause</button><button id="Resume1" onclick="return showPauseButton()">Resume</button><button id="Stop1" onclick="stopTasks()">Stop</button>
+        </li>
     </ul>
 </form>
 <!--ADD SUBJECT-->
-<form action="index.php" id="addToPlanning">
-    <label for="subject" class="label">Subject</label><br>
+<form action="index.php" id="addToPlanning" method="post">
+    <label for="subject" class="label">Subject</label>
     <select id="subject" name="subject">
         <option value="test1">test 1</option>
         <option value="test2">test 2</option>
         <option value="test3">test 3</option>
-    </select><br>
-    <label for="chapter" class="label">Chapter</label><br>
+    </select>
+    <label for="chapter" class="label">Chapter</label>
     <select id="chapter" name="chapter">
         <option value="test1">test 1</option>
         <option value="test2">test 2</option>
         <option value="test3">test 3</option>
-    </select><br>
+    </select>
+    <button id="planButton" type="submit" onclick="return false">Plan</button>
 </form>
 <br>
 <br>
-<button id="planButton">Plan</button>
 
 
+
+<script>
+    <!-- to hide pause button -->
+    let pause = document.getElementById("Pause1");
+    let resume = document.getElementById("Resume1");
+
+    function showPauseButton() {
+        pause.style.display = "inline";
+        resume.style.display = "none";
+        return false;
+    }
+
+    function hidePauseButton() {
+        resume.style.display = "inline";
+        pause.style.display = "none";
+        return false;
+    }
+    function stopTasks() {
+        <!--Stop tasks-->
+        if (confirm('Are you sure? This will remove the task.')) {
+            // Save it!
+            console.log("They're sure");
+        } else {
+            // Do nothing!
+            console.log("They're not sure");
+        }
+        return false;
+    }
+
+
+</script>
 
 
 </body>
