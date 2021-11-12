f0e880512b335c21dd86ed5025b30b915bcce3b5
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-11-12 09:11:02 +0100

    removed some useless files from the admin branch

--- template/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <img src="../Images/logo.png" alt="Logo" id="logo">
-    <ul id="header">
-        <li class="navbar"><a href="../Pre_planning/index.php">Overview</a></li>
-        <li class="navbar"><a href="../Submit/index.php">Submit</a></li>
-        <li class="navbar"><a href="../Admin/index.php">Admin</a></li>
-    </ul>
-</header>
