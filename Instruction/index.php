65ec6e6d268209e0561cbb550a5014abe497654d
Dylan Eckhart <deckhart@roc-dev.com>
2021-11-12 09:09:11 +0100

    Made instruction directory and files

---
+++ Instruction/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../Header/style.css">
+    <link rel="stylesheet" href="instruction_style.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <!-- Include header -->
+    <?php
+    require_once "../Header/header.php";
+    ?>
+
+    <!-- -->
+    <div id="grid">
+
+    </div>
+
+
+
+
+</body>
+</html>
