6acf6439d706f9a49054f61d1a83ed003b41af3a
Dylan Eckhart <deckhart@roc-dev.com>
2021-11-15 10:51:55 +0100

    Made text for the admin instruction

--- Instruction/index.php
+++ Instruction/index.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="../Header/style.css">
     <link rel="stylesheet" href="instruction_style.css">
+    <link rel="icon" href="../Images/icon/favicon.ico" type="image/icon">
     <title>Homepage</title>
 </head>
 <body>
@@ -22,19 +23,22 @@
     <div id="grid-container">
         <!-- Instruction block for submit -->
         <div class="instruction_submit">
-            <h2 class="submit_head">Submit</h2>
-            <img class="screenshot_submit" src="IMG/admin_screen.png">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed nisi lacus sed viverra tellus in hac habitasse. Elit eget gravida cum sociis natoque penatibus et magnis. Faucibus purus in massa tempor nec feugiat. Venenatis urna cursus eget nunc scelerisque viverra. Est velit egestas dui id ornare arcu odio. Leo in vitae turpis massa sed elementum. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Ultrices eros in cursus turpis massa tincidunt dui. Mus mauris vitae ultricies leo integer malesuada nunc. Nisl purus in mollis nunc sed id semper. Velit scelerisque in dictum non consectetur a. Viverra tellus in hac habitasse. Sagittis eu volutpat odio facilisis. Vitae tempus quam pellentesque nec. Velit ut tortor pretium viverra suspendisse potenti nullam. Porttitor rhoncus dolor purus non. Ut morbi tincidunt augue interdum velit
+            <h2 class="submit_head">Instruction of Submit</h2>
+            <img class="screenshot_submit" src="IMG/admin_screen.png" alt="screenshot">
+            <p>
+                
             </p>
         </div>
 
-        <!-- Instruction block for submit -->
+        <!-- Instruction block for admin -->
         <div class="instruction_admin">
-            <h2 class="admin_head">Admin</h2>
-            <img class="screenshot_admin" src="IMG/admin_screen.png">
+            <h2 class="admin_head">Instruction of Admin</h2>
+            <img class="screenshot_admin" src="IMG/admin_screen.png" alt="screenshot">
             <p>
-                euismod in pellentesque massa.
-                Sit amet massa vitae tortor condimentum. Velit laoreet id donec ultrices tincidunt. Elementum tempus egestas sed sed risus. Sed risus   ultrictristique nulla aliquet enim tortor at. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Id diam vel quam elementum pulvinar etiam. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Eleifend mi in nulla posuere. In massa tempor nec feugiat nisl pretium. Consequat nisl vel pretium lectus.
+                An admin can create tickets on this page for students to use. That works like this: <br>
+                - First the admin creates an category or chooses an existing one. <br>
+                - Then he creates an subject following the category or chooses one. <br>
+                - After that he creates a ticket with the following aspects: the category and subject you just made, a layer (front-end / back-end), a language                 that corresponds with the category, a small description of the task, deadline and a forcast time how long the student must work.
             </p>
         </div>
     </div>
