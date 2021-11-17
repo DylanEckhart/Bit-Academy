c29e372538adb7983a365d4fd88c4179affbafbd
Dylan Eckhart <deckhart@roc-dev.com>
2021-11-17 08:50:05 +0100

    Made the page responsive for almost every page

--- Instruction/index.php
+++ Instruction/index.php
@@ -26,7 +26,10 @@
             <h2 class="submit_head">Instruction of Submit</h2>
             <img class="screenshot_submit" src="IMG/admin_screen.png" alt="screenshot">
             <p>
-                
+                An admin can create tickets on this page for students to use. That works like this: <br>
+                - First the admin creates an category or chooses an existing one. <br>
+                - Then he creates an subject following the category or chooses one. <br>
+                - After that he creates a ticket with the following aspects: the category and subject you just made, a layer (front-end / back-end), a language that corresponds with                   the category, a small description of the task, deadline and a forcast time how long the student must work.
             </p>
         </div>
         <!-- Instruction block for admin -->
