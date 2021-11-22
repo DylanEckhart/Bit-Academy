b816c7eac81e0f5264cadd90b0b8fb4cebcaf57b
Dylan Eckhart <deckhart@roc-dev.com>
2021-11-22 11:51:14 +0100

    Fixed to display results from the database

--- Pre_planning/index.php
+++ Pre_planning/index.php
@@ -8,50 +8,99 @@
     <title>Previous Plannings</title>
 </head>
 <body>
-    <!-- Include Header -->
-    <?php
-        require_once "../Header/header.php";
-    ?>
+<!-- Include PHP files -->
+<?php
+    // Header
+    require_once "../Header/header.php";
 
-    <!-- Include database connection and ticketLoad -->
-    <?php
-        require_once '../DB/connection.php';
-        require_once 'ticketLoad.php';
-    ?>
+    // Database connection and variable
+    require_once '../DB/connection.php';
+    $connection = openConn();
 
-    <!-- Previous tickets of the students -->
-    <div id="PrePlanning">
-        <h1 id="pagetitle">Previous Plannings</h1>
-        <div id="PlanningGrid">
-            <div class="GridItem">
-                <div class="category">
-                    <div class="ticketList">
-                        <p class="ticket_title">PHP</p>
-                        <p class="ticket_desc">- Subject</p>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa sapien</p>
-                </div>
-                <div class="deadline">
-                    <p>Date:</p>
-                    <p>26-10-2021</p>
-                    <p>Deadline:</p>
-                    <p> 25-11-2021</p>
-                    <p class="deadlineFinished">Deadline finished:</p>
-                    <p>Yes or No</p>
-                    <span class="finishedColor"></span>
-                </div>
+    // Action
+    require_once 'ticketLoad.php';
+?>
 
-                <div class="time">
-                    <p>Scheduled time:</p>
-                    <p>5 hours</p>
-                    <p>Time spent:</p>
-                    <p>5:30 hours</p>
+<!-- Website Title -->
+<div id="PrePlanning">
+    <h1 id="pagetitle">Previous Plannings</h1>
+    <!-- Planning tickets of the student -->
+    <div id="PlanningGrid">
+        <div class="GridItem">
+            <div class="category">
+                <div class="ticketList">
+                    <?php
+                        // Query to select data from database
+                        $getDataQuery = "SELECT idplanning, categories_and_subjects_subjects_Subject, Description, Layer, Language, Start_Time, Deadline, Forcast_Time, TimeSpent, categories_Category, deadlineFinished, finishedInTime 
+                                         FROM tickets 
+                                         INNER JOIN categories_and_subjects 
+                                         ON tickets.categories_and_subjects_subjects_Subject = categories_and_subjects.subjects_Subject 
+                                         INNER JOIN plannings 
+                                         ON tickets.Description = plannings.tickets_Description 
+                                         WHERE IsFinished = 1";
+
+                        // Get query results
+                        $setData = mysqli_query($connection, $getDataQuery);
+
+                        while ($row = mysqli_fetch_assoc($setData)) {
+                            $dataArray[] = $row;
+                        }
+
+                        // Display category and subject
+                        if (sizeof($dataArray) > 0) {
+                            foreach ($dataArray as $category) {
+                                echo '<p class="ticket_title">' . $category["categories_Category"] . ' - </p>';
+                                echo '<p class="ticket_title">' . $category["categories_and_subjects_subjects_Subject"] . '</p>';
+
+                            }
+                        } else echo "<option disabled hidden selected>There are no categories yet</option>";
+                    ?>
                 </div>
+                <!-- Display description -->
+                <?php
+                    if (sizeof($dataArray) > 0 ) {
+                        foreach ($dataArray as $description) {
+                            echo '<p>' . $description["Description"] . '</p>';
+                        }
+                    }
+                ?>
+            </div>
+
+            <div class="deadline">
+            <!-- Display date, deadline and deadline finished -->
+                <?php
+                    if (sizeof($dataArray) > 0) {
+                        foreach ($dataArray as $deadline) {
+                            echo '<p>Date:</p>';
+                            echo '<p>' . $deadline["Start_Time"] . '</p>';
+                            echo '<p>Deadline:</p>';
+                            echo '<p>' . $deadline["Deadline"] . '</p>';
+                            echo '<p class="deadlineFinished">Deadline finished:</p>';
+                            echo '<p>' . ($deadline["deadlineFinished"] ? "Yes" : "No") . '</p>';
+                        }
+                    }
+                ?>
+                <span class="finishedColor"></span>
+            </div>
+
+            <div class="time">
+                <!-- Display forcast time and time spent -->
+                <?php
+                    if (sizeof($dataArray) > 0) {
+                        foreach ($dataArray as $time) {
+                            echo '<p>Forcast time:</p>';
+                            echo '<p>' . $time["Forcast_Time"] . '</p>';
+                            echo '<p>Time spent:</p>';
+                            echo '<p>' . $time["TimeSpent"] . '</p>';
+                            echo '<p>Finished in time:</p>';
+                            echo '<p>' . ($time["finishedInTime"] ? "Yes" : "No") . '</p>';
+                        }
+                    }
+                ?>
+                <span class="finishedColor"></span>
             </div>
         </div>
     </div>
-    <script>
-
-    </script>
+</div>
 </body>
 </html>
