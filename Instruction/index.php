0f0647ef5ad4e5771bc57ab25b2f2ddd62024169
Rick <Mcfly2340@outlook.com>
2021-11-22 12:53:08 +0100

    fixed some typos

--- Instruction/index.php
+++ Instruction/index.php
@@ -26,11 +26,11 @@
             <h2 class="submit_head">Instruction of Submit</h2>
             <img class="screenshot_submit" src="IMG/submit_screen.png" alt="screenshot">
             <p>
-                An student can create a planning on this page to organize his work. That works like this: <br>
-                - First the student chooses an category. <br>
-                - Then chooses an subject that corresponds with the category. <br>
-                - After that chooses the description and plan his planning. <br>
-                - The planning will stand at the right of the screen.
+                A student can create a planning on this page to organize their work. It works like this: <br>
+                - First the student chooses a category. <br>
+                - Then chooses a subject that corresponds with the category. <br>
+                - After that the student chooses the description and adds it to their planning. <br>
+                - The ticket will display at the right side of the screen under This Week Planning.
             </p>
         </div>
         <!-- Instruction block for admin -->
@@ -38,10 +38,10 @@
             <h2 class="admin_head">Instruction of Admin</h2>
             <img class="screenshot_admin" src="IMG/admin_screen.png" alt="screenshot">
             <p>
-                An admin can create tickets on this page for students to use. That works like this: <br>
-                - First the admin creates an category or chooses an existing one. <br>
-                - Then he creates an subject following the category or chooses one. <br>
-                - After that he creates a ticket with the following aspects: the category and subject you just made, a layer (front-end / back-end), a language that corresponds with                   the category, a small description of the task, deadline and a forcast time how long the student must work.
+                An admin can create tickets on this page for students to use. It works like this: <br>
+                - First the admin creates a category or chooses an existing one. <br>
+                - Then they can create a subject while a category is being selected. <br>
+                - After that they can create a ticket with the following aspects: the category and subject you just made, a layer (front-end / back-end), a language that corresponds with the category, a small description of the task, deadline and a forcast time how long the student must work.
             </p>
         </div>
     </div>
