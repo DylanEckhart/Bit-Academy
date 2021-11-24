68d5ee55313f04ed47d9cd427a0be33b0235882a
Rick <Mcfly2340@outlook.com>
2021-11-24 09:57:40 +0100

    changed header's navbar to responsive & delete button changes

--- Header/header.php
+++ Header/header.php
@@ -1,9 +1,10 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <header class="topnav" id="myTopnav">
     <img src="../Images/logo.png" alt="Logo" id="logo" title="Go to the Homepage">
 </header>
 
 <div class="nav-links" id="navlinks">
-    <a href="javascript:void(0);" style="font-size:25px;" id="menuButtonOpen" onclick="hideMenu()">&#10006;</a>
+    <a href="javascript:void(0);" id="menuButtonOpen" class="menuButton" onclick="hideMenu()">&#10006;</a>
     <ul id="header">
         <li class="navbar"><a href="../Instruction/index.php" class="navItem">Home</a> </li>
         <li class="navbar"><a href="../Pre_planning/index.php" class="navItem">Previous Planning</a></li>
@@ -12,7 +13,7 @@
     </ul>
 </div>
 <div>
-<a href="javascript:void(0);" style="font-size:25px;" id="menuButtonClosed" onclick="showMenu()">&#9776;</a>
+<a href="javascript:void(0);" id="menuButtonClosed" onclick="showMenu()">&#9776;</a>
 </div>
 
 
